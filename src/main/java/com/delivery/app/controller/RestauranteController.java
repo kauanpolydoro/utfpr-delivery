605c177773cc7f755593a0964842d72c1dc6dd5e
Kauan Polydoro <kauan.2812@hotmail.com>
2023-03-11 17:33:10 -0300

    feature: Validation errors

--- src/main/java/com/delivery/app/controller/RestauranteController.java
+++ src/main/java/com/delivery/app/controller/RestauranteController.java
@@ -4,20 +4,24 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.delivery.app.dto.RestauranteInputDTO;
 import com.delivery.app.dto.RestauranteResumidoDTO;
 import com.delivery.app.entity.Restaurante;
+import com.delivery.app.mapper.RestauranteInputMapper;
 import com.delivery.app.mapper.RestauranteResumidoMapper;
 import com.delivery.app.service.RestauranteService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -27,9 +31,12 @@ public class RestauranteController {
 	private RestauranteService restauranteService;
 
 	@Autowired
+	private RestauranteInputMapper restauranteInputMapper;
+
+	@Autowired
 	private RestauranteResumidoMapper restauranteResumidoMapper;
 
-	@GetMapping("/")
+	@GetMapping
 	@ResponseBody
 	public List<RestauranteResumidoDTO> listarRestaurantes() {
 
@@ -50,10 +57,23 @@ public class RestauranteController {
 
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public Restaurante create(@RequestBody Restaurante restaurante) {
+	public Restaurante create(@RequestBody @Valid RestauranteInputDTO restauranteInputDTO) {
+
+		Restaurante restaurante = restauranteInputMapper.map(restauranteInputDTO);
 
 		return restauranteService.create(restaurante);
 
+	}
+
+	@PutMapping("/{id}")
+	@ResponseBody
+	private Restaurante update(@PathVariable Long id, @RequestBody @Valid RestauranteInputDTO restauranteInputDTO) {
+		
+		Restaurante restaurante = restauranteInputMapper.map(restauranteInputDTO);
+		
+		restaurante = restauranteService.update(id, restaurante);
+
+		return restaurante;
 	}
 
 }
--- src/main/java/com/delivery/app/handler/ApiExceptionHandler.java
+++ src/main/java/com/delivery/app/handler/ApiExceptionHandler.java
@@ -1,8 +1,17 @@
 package com.delivery.app.handler;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -13,16 +22,58 @@ import com.delivery.app.exception.NotFoundException;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
-    
+	
+	@Autowired
+	private MessageSource messageSource;
+
 	@ExceptionHandler(NotFoundException.class)
-    protected ResponseEntity<Object> handleNotFoundException(NotFoundException ex, WebRequest request) {
-        ExceptionResponseDTO exceptionResponse = new ExceptionResponseDTO();
+	protected ResponseEntity<Object> handleNotFoundException(NotFoundException ex, WebRequest request) {
+		ExceptionResponseDTO exceptionResponse = new ExceptionResponseDTO();
 
-        exceptionResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        exceptionResponse.setMensagem(ex.getLocalizedMessage());
+		exceptionResponse.setStatus(HttpStatus.NOT_FOUND.value());
+		exceptionResponse.setMensagem(ex.getLocalizedMessage());
 
-        return handleExceptionInternal(ex, exceptionResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
-    }
+		return handleExceptionInternal(ex, exceptionResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
 
+	@ExceptionHandler(RuntimeException.class)
+	protected ResponseEntity<Object> handleRunTimeException(RuntimeException ex, WebRequest request) {
+		ExceptionResponseDTO exceptionResponse = new ExceptionResponseDTO();
+
+		exceptionResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		exceptionResponse.setMensagem("Serviço temporariamente indisponível");
+
+		return handleExceptionInternal(ex, exceptionResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR,
+				request);
+	}
+
+	@Override
+	@Nullable
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+		return handleValidationInternal(ex, request, ex.getBindingResult());
+	}
+
+	private ResponseEntity<Object> handleValidationInternal(Exception ex,  WebRequest request, BindingResult bindingResult) {
+		
+		List<ExceptionResponseDTO.Erro> erros = bindingResult.getFieldErrors().stream().map(field -> {
+			String message = messageSource.getMessage(field, LocaleContextHolder.getLocale());
+			
+			ExceptionResponseDTO.Erro erro = new ExceptionResponseDTO.Erro();
+			erro.setCampo(field.getField());
+			erro.setErro(message);
+			
+			return erro;
+		}).toList();
+		
+		ExceptionResponseDTO response = new ExceptionResponseDTO();
+		
+		response.setStatus(HttpStatus.BAD_REQUEST.value());
+		response.setMensagem("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente!");
+		response.setValidacoes(erros);
+		
+		return handleExceptionInternal(ex, response, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+
+	}
 }
-
